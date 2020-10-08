149de1ffe15f71539dcfbb3f29c4f400b83a8e0b
Ubuntu <ubuntu@ip-172-31-91-213.ec2.internal>
2020-10-08 22:32:43 +0000

    Update 3

--- chooch_service.py
+++ chooch_service.py
@@ -20,6 +20,10 @@ if __name__ == '__main__':
     
     # run docker
     
+    
+   
+    
+    
             # first read device_info
     with open("{}/data/config.json".format(app_path)) as f:        
             json_data = json.load(f)
@@ -29,7 +33,17 @@ if __name__ == '__main__':
     mount_path = app_path + "/data"
     
     
-    docker_command = "docker run --runtime nvidia  --rm  -v {}:/app/chooch_ai_on_prem/data -p 8000:8000  {} python3 chooch_service.py".format( mount_path, docker_url)
+     # pull docker
+        
+    docker_command_pull = "docker pull {}".format(docker_url)
+    
+    print(docker_command_pull)                                                                                                                             
+    os.system(docker_command_pull)
+        
+        
+    # run docker
+    
+    docker_command = "docker run --runtime nvidia  --rm  -v {}:/app/chooch_ai_on_prem/data -p 8000:8000  {} python3 chooch_service.py".format(mount_path, docker_url)
     
                                                                                                                                    
     print(docker_command)                                                                                                                             
